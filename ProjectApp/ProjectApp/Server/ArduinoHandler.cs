e8aa20707fb1651acf2efc1483eea71403d03798
Guerzoniansus <weezimonkey@gmail.com>
2020-06-09 14:54:39 +0200

    Removed some Opdracht A specific code

--- ProjectApp/ProjectApp/Server/ArduinoHandler.cs
+++ ProjectApp/ProjectApp/Server/ArduinoHandler.cs
@@ -13,23 +13,13 @@ namespace ProjectApp.Server
     class ArduinoHandler
     {
         /// <summary>ArduinoStatus object that can be used as a DataBinding object for the user interface to automatically update and display data.</summary>
-        public ArduinoStatus Status { get; private set; }
-
         private Connection connection;
+
         
         /// <summary>Returns an ArduinoHandler object. Invoke StartConnection() to start a connection with the server.</summary>
         public ArduinoHandler()
         {
             connection = new Connection();
-            Status = new ArduinoStatus()
-            {
-                ConnectionStatus = "Not connected",
-                PinState = "-",
-                SensorValue = "-",
-                SensorCriteria = "-",
-                InfoText = "-",
-                CriteriaMode = "-"
-            };
         }
 
         /// <summary>
@@ -41,36 +31,27 @@ namespace ProjectApp.Server
         /// <returns></returns>
         public bool StartConnection(string ipAddress, string port)
         {
-            if (connection.Connected == true)
+            if (connection.IsConnected())
                 return false;
 
             // Try to start connection
             try
             {
-                //Console.WriteLine("IP: " + ipAddress);
-                //Console.WriteLine("PORT: " + port);
-
                 // Start connection
                 connection.StartConnection(ipAddress, port);
 
-                Status.InfoText = "-";
-
-                Device.InvokeOnMainThreadAsync(() =>
-                {
-                    Status.PinState = GetPinState();
-                    Status.SensorCriteria = GetSensorCriteria();
-                    Status.CriteriaMode = GetCriteriaMode();
-                });
+                // Initialize values
+                RefreshStatus();
             }
 
+            // Something went wrong (like invalid IP or port)
             catch (ArgumentException e)
             {
-                Status.InfoText = e.Message;
+                return false;
             }
 
-            Status.ConnectionStatus = connection.Connected ? "Connected" : "Not connected";
 
-            return Status.ConnectionStatus == "Connected";
+            return connection.IsConnected();
         }
 
         /// <summary>
@@ -78,26 +59,15 @@ namespace ProjectApp.Server
         /// </summary>
         public void RefreshStatus()
         {
-            // Set connected 
-            Status.ConnectionStatus = connection.Connected ? "Connected" : "Not connected";
+            // Not used right now    
+        }
 
-            if (connection.Connected) {
-                // Set sensor value (and info text)
-                string sensorValue = GetSensorValue();
-
-                if (sensorValue == "error")
-                {
-                    Status.InfoText = "Could not retrieve sensor data";
-                    Status.SensorValue = "-";
-                }
-
-                else Status.SensorValue = sensorValue;
-
-                if (Status.CriteriaMode == "auto")
-                {
-                    Status.PinState = GetPinState();
-                }
-            }          
+        /// <summary>
+        /// Returns whether or not there is a connection with the Arduino going on. 
+        /// </summary>
+        public bool IsConnected()
+        {
+            return connection.IsConnected();
         }
 
         /* =========================================================================
@@ -117,17 +87,13 @@ namespace ProjectApp.Server
         /* Command List - Put a list of commands here so it's easy to look up when coding the Arduino responses
          * Format: Method - Command - Expected Arduino response (NOT method return value)
          * 
+         * Example:
          * GetSensorValue()     - "s" - Value
-         * GetPinState()        - "p" - "ON", "OFF"
-         * GetSensorCriteria()  - "c" - Sensor criteria number
-         * GetCriteriaMode()    - "m" - "00a" (auto) or "00m" (manual)
          * 
-         * TogglePinState()     - "t" - "ON" or "OFF" (the new state)
-         * SetSensorCriteria()  - "s=X" where x is a number - "sOk"
-         * SetCriteriaMode()    - "ma" or "mm" (for Mode Automatic, or Mode Manual) - "00m" or "00a" (the NEW mode)
          */
 
 
+        // Example:
 
         /// <summary>
         /// Returns the value from the sensor, or "error" if it could not be obtained.
@@ -138,115 +104,10 @@ namespace ProjectApp.Server
             // Command = "s" (for Sensor)
             // Response = a number, or "error"
 
+            // Returns sensor value, or "error" if it could not be retrieved
             return connection.ExecuteCommand("s");
         }
 
-        /// <summary>
-        /// Returns "ON" or "OFF" or "error" if the pin state could not be obtained. 
-        /// </summary>
-        /// <returns></returns>
-        public string GetPinState()
-        {
-            // Command = "p" (for Pinstate)
-            // Response = "On", "Off", or "error"
-            return connection.ExecuteCommand("p");
-        }
-
-        /// <summary>
-        /// Returns the Sensor Criteria number or "error". 
-        /// </summary>
-        /// <returns></returns>
-        public string GetSensorCriteria()
-        {
-            // Command = "c"
-            // Response = number
-            return connection.ExecuteCommand("c");
-        }
-
-        /// <summary>
-        /// Get the Criteria Mode. Returns "auto" if the Arduino automatically activates based on sensor criteria, "manual" if the user can activate using the app, or "error".
-        /// </summary>
-        /// <returns></returns>
-        public string GetCriteriaMode()
-        {
-            // Command = "m" (for criteria Mode)
-            // Response = "00a" (auto) or "00m" (manual) (00 because of dumb 4 char encoding stuff)
-
-            string response = connection.ExecuteCommand("m");
-
-            if (response == "00a") 
-                return "auto";
-
-            else if (response == "00m") 
-                return "manual";
-
-            else return response;
-        }
-
-        /// <summary>
-        /// Toggles the pin state and returns the new state (ON or OFF), or "error" if the state could not be changed. 
-        /// </summary>
-        /// <returns></returns>
-        public string TogglePinState()
-        {
-
-            // Command = "t" (for Toggle)
-            // Response = "ON" or "OFF" (the new changed pin)
-
-            string response = connection.ExecuteCommand("t");
-
-            Status.PinState = response;
-            return response;
-        }
-
-        /// <summary>
-        /// Sets the criteria for when Arduino needs to turn pins ON when it reaches this value.
-        /// Returns "error" if something went wrong.
-        /// </summary>
-        /// <param name="criteria">The sensor criteria that should be met on the Arduino.</param>
-        /// <returns></returns>
-        public string SetSensorCriteria(int criteria)
-        {
-            // int is used to make sure it's a valid number
-
-            // Command = "s=X" where X is the number for the sensor criteria, like "s=100"
-            // Response = "sOk" if the critera is set, or "error" if it could not be set
-
-            string response = connection.ExecuteCommand("s=" + criteria);
-
-            if (response == "sOk")
-            {
-                Status.SensorCriteria = Convert.ToString(criteria);
-            }
-
-            return response;
-        }
-
-        /// <summary>
-        /// Set the Criteria Mode to "a" (auto) or "m" (manual). Returns the new mode, or "error" if the mode could not be changed. 
-        /// </summary>
-        /// <exception cref="ArgumentException">Thrown when mode does not equal "a" or "m".</exception>
-        /// <param name="mode">The new mode, must equal "a" or "m".</param>
-        /// <returns></returns>
-        public string SetCriteriaMode(string mode)
-        {
-            // Command = ma or mm (for Mode Automatic, or Mode Manual)
-            // Response = "00m" or "00a" (the NEW criteria mode)
-
-            if (mode != "a" && mode != "m")
-            {
-                throw new ArgumentException("Mode must specifically equal 'a' or 'm'.");
-            }
-
-            string response = connection.ExecuteCommand("m" + mode);
-
-            if (response == "00a" || response == "00m")
-            {
-                Status.CriteriaMode = response == "00a" ? "auto" : "manual";
-            }
-
-            return response;
-        }
 
     }
 }
