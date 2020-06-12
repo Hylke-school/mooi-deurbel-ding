dc02a0db7df97190cae5ab3ff064882a0c2e44ae
Guerzoniansus <weezimonkey@gmail.com>
2020-06-12 13:57:16 +0200

    Finished database code

---
+++ ProjectApp/ProjectApp.Android/DatabaseService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ProjectApp.Database;
+using ProjectApp.Droid;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(DatabaseService))]
+namespace ProjectApp.Droid
+{
+    public class DatabaseService : IDBInterface
+    {
+        public SQLiteConnection CreateConnection()
+        {
+            var sqliteFilename = "HistoryDatabase.db";
+            string documentsDirectoryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsDirectoryPath, sqliteFilename);
+
+            if (!File.Exists(path))
+            {
+                using (var binaryReader = new BinaryReader(Android.App.Application.Context.Assets.Open(sqliteFilename)))
+                {
+                    using (var binaryWriter = new BinaryWriter(new FileStream(path, FileMode.Create)))
+                    {
+                        byte[] buffer = new byte[2048];
+                        int length = 0;
+
+                        while ((length = binaryReader.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            binaryWriter.Write(buffer, 0, length);
+                        }
+                    }
+                }
+            }
+
+            var conn = new SQLiteConnection(path, false);
+
+            return conn;
+        }
+    }
+}
---
+++ ProjectApp/ProjectApp/Database/Date.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectApp.Database
+{
+    [Table("Date")]
+    class Date
+    {
+
+        [PrimaryKey, AutoIncrement, Unique, NotNull, Column("ID")]
+        public int ID { get; set; }
+
+        [PrimaryKey, NotNull, Column("date")]
+        public string date { get; set; }
+    }
+}
